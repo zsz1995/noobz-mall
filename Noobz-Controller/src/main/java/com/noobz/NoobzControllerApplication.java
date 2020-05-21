7b3c3ef4a97303553b9109534dfc9e7176bb15e9
zsz1995 <13687646803>
2020-05-21 14:46:03 +0800

    war SpringbootServletInitializer

--- Noobz-Controller/src/main/java/com/noobz/NoobzControllerApplication.java
+++ Noobz-Controller/src/main/java/com/noobz/NoobzControllerApplication.java
@@ -2,9 +2,16 @@ package com.noobz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NoobzControllerApplication {
+public class NoobzControllerApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(NoobzControllerApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(NoobzControllerApplication.class, args);
--- Noobz-User/src/main/java/com/noobz/user/NoobzUserApplication.java
+++ Noobz-User/src/main/java/com/noobz/user/NoobzUserApplication.java
@@ -2,9 +2,16 @@ package com.noobz.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NoobzUserApplication {
+public class NoobzUserApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(NoobzUserApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(NoobzUserApplication.class, args);
