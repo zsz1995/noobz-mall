b03c807b6b5305620658661d7e4b11ffce0cfa87
zsz1995 <13687646803>
2020-05-21 10:44:20 +0800

    common service application

---
+++ Noobz-Common-Service-Facade/src/main/java/com/noobz/NoobzCommonServiceFacadeApplication.java
@@ -0,0 +1,16 @@
+package com.noobz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhousz
+ * @date 2020/5/21 10:43
+ */
+@SpringBootApplication
+public class NoobzCommonServiceFacadeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoobzCommonServiceFacadeApplication.class, args);
+  }
+}
